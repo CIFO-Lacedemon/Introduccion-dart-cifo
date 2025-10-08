8f0f3412b94bcc3744c04c98148c8725fd85905a
JoseManuelBarba <josemanuelbarbasanchez@gmail.com>
2025-10-08 12:18:43 +0200

    funciones basicas

---
+++ 05_funciones/01_funciones_basicas.dart
@@ -0,0 +1,70 @@
+// void saludar() {}
+
+String saludar() {
+  int numero1 = 10;
+  int numero2 = 20;
+
+  print(numero1 + numero2);
+
+  String mensaje = "Hola Mundo";
+
+  return mensaje;
+}
+
+int sumar() {
+  int numero1 = 10;
+  int numero2 = 20;
+
+  return numero1 + numero2;
+}
+
+void saludaSinRetorno() {
+  print("Holaaaa!!!!");
+}
+
+int nuevoValor = 200;
+
+int sumarConArgumento(int? numero1, int numero2) {
+  // int numero1 = 10;
+  // int numero2 = 20;
+
+  return numero1 ?? 0 + numero2;
+}
+
+int sumarConNombre({
+  required int numero1,
+  int? numero2,
+  required int? numero3,
+}) {
+  return numero1 + (numero2 ?? 0);
+}
+
+void saludarEtiqueta(String mensaje, [String? nombre = "<Insertar algo>"]) {
+  print('$mensaje    $nombre');
+}
+
+void main() {
+  String mensaje = saludar();
+
+  int resultado = sumar();
+
+  print(mensaje);
+
+  print(resultado);
+
+  saludaSinRetorno();
+
+  mensaje = "Nuevo Mensaje";
+
+  print(mensaje);
+
+  int resultado2 = sumarConArgumento(null, 20);
+
+  print(resultado2);
+
+  int resultado3 = sumarConNombre(numero3: null, numero1: 10);
+
+  print(resultado3);
+
+  saludarEtiqueta('Hola mundo');
+}
