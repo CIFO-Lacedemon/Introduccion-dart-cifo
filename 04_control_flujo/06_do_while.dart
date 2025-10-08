0a30d868700dd619f85202febf5d935fa094accc
JoseManuelBarba <josemanuelbarbasanchez@gmail.com>
2025-10-08 11:11:43 +0200

    do while

--- 04_control_flujo/06_do_while.dart
+++ 04_control_flujo/06_do_while.dart
@@ -3,11 +3,15 @@ import 'dart:io';
 void main() {
   String continuar = 'n';
 
+  int? contadorEntradaPorTeclado;
+
   int contador = 0;
 
   do {
     print(continuar);
     contador++;
+
+    contador = contadorEntradaPorTeclado ?? 0;
     print(contador);
     stdout.writeln('¿Desea continuar? (y/n)');
     continuar = stdin.readLineSync()!;
